7db9a6718a9b2ec78ea209711355423b5741078a
jcouraud <20691516+jcouraud@users.noreply.github.com>
2021-12-10 13:05:46 -0700

    Setting up stock class for initial work

--- includes/stocks.php
+++ includes/stocks.php
@@ -18,39 +18,12 @@ class stocks {
     
     //Class function to save stock symbol to database
     public function saveStock($symbol){
-        try{
-            $stmt = $this->db->prepare("INSERT INTO stocks(stock_symbol) VALUES (:stock_symbol)");
-
-            $stmt->bindparam(":stock_symbol", $symbol);
-
-            $stmt->execute();
-            $stmt->closeCursor();
-            
-            if($stmt->rowCount() != 0){
-                return 1;
-            }else{
-                return 0;
-            }
-        } catch (Exception $ex) {
-            return 0;
-        }
+        
     }
     
     //Class function to load all stocks from database
     public function loadAllStocks(){
-        try{
-            $stmt = $this->db->prepare("SELECT * FROM stocks");
-            $stmt->execute();
-
-            if($stmt->rowCount() > 0){
-                $this->symbols = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            }
-            
-            $stmt->closeCursor();
-            
-        } catch (Exception $ex) {
-            return 0;
-        }
+        
     }
     
     //Class function to access the stocks variable from the class
@@ -112,31 +85,11 @@ class stocks {
     
     //Class function to format the retrieved stock data for graph display
     private function formatStockData($results){
-        $counter = 0;
-        foreach ($results as $result){
-            
-                $this->stocks[$result->symbol]['symbol'] = $result->symbol;
-                if($counter < 12){
-                    $this->stocks[$result->symbol]['data'][] = "['" . $result->period . "', " . $result->strongBuy . ", " . $result->buy . ", " . $result->hold . ", " . $result->sell . ", " . $result->strongSell . "]";
-                $counter++;
-            }
-        }
+        
     }
     
     //Class funtion to delete stock from database
     public function deleteStock($symbol){
-        try {
-            $stmt = $this->db->prepare("DELETE FROM stocks WHERE stock_symbol = :stock_symbol");
-
-            $stmt->bindparam(":stock_symbol", $symbol);
-
-            $stmt->execute();
-            $rowCount = $stmt->rowCount();
-            $stmt->closeCursor();
-            
-            return $rowCount;
-        } catch (Exception $ex) {
-            return 0;
-        }
+        
     }
 }
